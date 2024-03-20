370bc77371d7c212f109fed1a868eac4033496c3
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-03-19 21:33:53 -0500

    updated bonu

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    // Check if the query parameter 'x' exists
     var x = req.query.x ? parseFloat(req.query.x) : Math.random() * (10 - (-1)) + (-1);
     var y = Math.cosh(x);
     res.send(`cosh[${x}] is [${y}]`);
